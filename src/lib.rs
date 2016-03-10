08a6dac3749bb64feec50641148814f33c075b0c
Ilkka Halila <ilkka@hali.la>
2016-03-10 20:34:10 +0200

    Added function bindings for the functions that start with 'c'

--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,7 @@ use libc::{c_char, c_int, c_short, c_void};
 
 pub type chtype = u64; // chtypes 64 bits by default
 pub type attr_t = chtype;
-pub type PDCursesBool = u8;
+pub type pdc_bool = u8;
 
 #[repr(C)]
 #[derive(Clone, Copy)]
@@ -19,13 +19,13 @@ pub struct WINDOW {
     pub _flags: i32, // window properties
     pub _attrs: chtype, // standard attributes and colors
     pub bkgd: chtype, // background, normally blank
-    pub _clear: PDCursesBool, // causes clear at next refresh
-    pub _leaveit: PDCursesBool, // leaves cursor where it is
-    pub _scroll: PDCursesBool, // allows window scrolling
-    pub _nodelay: PDCursesBool, // input character wait flag
-    pub _immed: PDCursesBool, // immediate update flag
-    pub _sync: PDCursesBool, // synchronise window ancestors
-    pub _use_keypad: PDCursesBool, // flags keypad key mode active
+    pub _clear: pdc_bool, // causes clear at next refresh
+    pub _leaveit: pdc_bool, // leaves cursor where it is
+    pub _scroll: pdc_bool, // allows window scrolling
+    pub _nodelay: pdc_bool, // input character wait flag
+    pub _immed: pdc_bool, // immediate update flag
+    pub _sync: pdc_bool, // synchronise window ancestors
+    pub _use_keypad: pdc_bool, // flags keypad key mode active
     pub _y: *mut *mut chtype, // pointer to line pointer array
     pub _firstch: *mut i32, // first changed character in line
     pub _lastch: *mut i32, // last changed character in line
@@ -75,8 +75,27 @@ extern "C" {
                   -> c_int;
     pub fn pdc_box(_: *mut WINDOW, _: chtype, _: chtype) -> c_int;
 
-
+    pub fn can_change_color() -> pdc_bool;
     pub fn cbreak() -> c_int;
+    pub fn chgat(_: c_int, _: attr_t, _: c_short, _: *const c_void) -> c_int;
+    pub fn clearok(_: *mut WINDOW, _: pdc_bool) -> c_int;
+    pub fn clear() -> c_int;
+    pub fn clrtobot() -> c_int;
+    pub fn clrtoeol() -> c_int;
+    pub fn color_content(_: c_short, _: *mut c_short, _: *mut c_short, _: *mut c_short) -> c_int;
+    pub fn color_set(_: short, _: *mut c_void) -> c_int;
+    pub fn copywin(_: *const WINDOW,
+                   _: *mut WINDOW,
+                   _: c_int,
+                   _: c_int,
+                   _: c_int,
+                   _: c_int,
+                   _: c_int,
+                   _: c_int,
+                   _: c_int)
+                   -> c_int;
+    pub fn curs_set(_: c_int) -> c_int;
+
     pub fn echo() -> c_int;
     pub fn endwin() -> c_int;
     pub fn getch() -> c_int;
