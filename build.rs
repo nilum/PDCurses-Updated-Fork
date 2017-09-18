62eb6bdb288e1da0149d1bdcbcceaf4a9a215d16
Ilkka Halila <ilkka@hali.la>
2017-09-18 17:33:16 +0300

    gcc::Config has been renamed to gcc::Build

--- build.rs
+++ build.rs
@@ -5,7 +5,7 @@ fn main() {
     println!("cargo:rustc-link-lib=dylib=comdlg32");
     println!("cargo:rustc-link-lib=dylib=user32");
 
-    gcc::Config::new()
+    gcc::Build::new()
         .file("src/PDCurses/pdcurses/addch.c") //Common PDCurses files
         .file("src/PDCurses/pdcurses/addchstr.c")
         .file("src/PDCurses/pdcurses/addstr.c")
